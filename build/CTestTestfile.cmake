326a2021041c424268934b735fe28f4db630f8a0
Tao <528164177@qq.com>
2023-08-25 17:19:41 +0800

    modify

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/acer/taowebserver
-# Build directory: /root/acer/taowebserver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
